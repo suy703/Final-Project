958107f329ec60d165bc843bef4ac39cb8c520d1
suy703 <suy@ggc.edu>
2017-04-18 23:31:18 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -163,10 +163,87 @@ public class Driver extends Application implements ClientInfo {
 		title.setLayoutX(230);
 		title.setLayoutY(27);
 		
-		// add Labels, TextFields, ChoiceBox, & CheckBox HERE
-		//
-		//
-		//
+		Label driver1Name = new Label("Driver #1 Name:");
+		driver1Name.setLayoutX(14);
+		driver1Name.setLayoutY(66);
+		Label driver2Name = new Label("Driver #2 Name:");
+		driver2Name.setLayoutX(14);
+		driver2Name.setLayoutY(130);
+		Label driver3Name = new Label("Driver #3 Name:");
+		driver3Name.setLayoutX(14);
+		driver3Name.setLayoutY(194);
+		
+		Label dob1 = new Label("Date of Birth:");
+		dob1.setLayoutX(152);
+		dob1.setLayoutY(66);
+		Label dob2 = new Label("Date of Birth:");
+		dob2.setLayoutX(152);
+		dob2.setLayoutY(130);
+		Label dob3 = new Label("Date of Birth:");
+		dob3.setLayoutX(152);
+		dob3.setLayoutY(194);
+
+		Label license1 = new Label("License #:");
+		license1.setLayoutX(256);
+		license1.setLayoutY(66);
+		Label license2 = new Label("License #:");
+		license2.setLayoutX(256);
+		license2.setLayoutY(130);
+		Label license3 = new Label("License #:");
+		license3.setLayoutX(256);
+		license3.setLayoutY(194);
+	
+		Label age1 = new Label("Age First License:");
+		age1.setLayoutX(359);
+		age1.setLayoutY(66);
+		Label age2 = new Label("Age First License:");
+		age2.setLayoutX(359);
+		age2.setLayoutY(130);
+		Label age3 = new Label("Age First License:");
+		age3.setLayoutX(359);
+		age3.setLayoutY(194);
+
+		Label status1 = new Label("Marital Status:");
+		status1.setLayoutX(460);
+		status1.setLayoutY(66);
+		Label status2 = new Label("Marital Status:");
+		status2.setLayoutX(460);
+		status2.setLayoutY(130);
+		Label status3 = new Label("Marital Status:");
+		status3.setLayoutX(460);
+		status3.setLayoutY(194);
+
+		TextField driver1Text = new TextField();
+		driver1Text.setLayoutX(14);
+		driver1Text.setLayoutY(81);
+		driver1Text.setPrefWidth(122);
+		driver1Text.setPrefHeight(25);
+		TextField driver2Text = new TextField();
+		driver2Text.setLayoutX(14);
+		driver2Text.setLayoutY(145);
+		driver2Text.setPrefWidth(122);
+		driver2Text.setPrefHeight(25);
+		TextField driver3Text = new TextField();
+		driver3Text.setLayoutX(14);
+		driver3Text.setLayoutY(209);
+		driver3Text.setPrefWidth(122);
+		driver3Text.setPrefHeight(25);
+		
+		TextField dob1Text = new TextField();
+		dob1Text.setLayoutX(152);
+		dob1Text.setLayoutY(81);
+		dob1Text.setPrefWidth(88);
+		dob1Text.setPrefHeight(25);
+		TextField dob2Text = new TextField();
+		dob2Text.setLayoutX(152);
+		dob2Text.setLayoutY(145);
+		dob2Text.setPrefWidth(88);
+		dob2Text.setPrefHeight(25);
+		TextField dob3Text = new TextField();
+		dob3Text.setLayoutX(152);
+		dob3Text.setLayoutY(209);
+		dob3Text.setPrefWidth(88);
+		dob3Text.setPrefHeight(25);
 		
 		//BACK BUTTON
 		Button back = new Button("Back");
@@ -188,7 +265,9 @@ public class Driver extends Application implements ClientInfo {
 		menuFile.getItems().add(exit);
 		
 		topPane.getChildren().add(menuBar);
-		centerPane.getChildren().addAll(title);
+		centerPane.getChildren().addAll(title, driver1Name, driver2Name, driver3Name, dob1, dob2, dob3, license1,
+				license2, license3, age1, age2, age3, status1, status2, status3, driver1Text, driver2Text,
+				driver3Text, dob1Text, dob2Text, dob3Text);
 		bottomPane.getChildren().addAll(back, submit);
 		scene3 = new Scene(borderPane);
 	}
